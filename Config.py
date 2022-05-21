a7e345e9d9ddfbc039f7ecf5965179f4f972d269
SAIF ALI <80207551+saifalisew1508@users.noreply.github.com>
2022-05-21 09:32:50 +0530

    Create Config.py

---
+++ Config.py
@@ -0,0 +1,16 @@
+import re
+import sys
+from os import getenv
+
+from dotenv import load_dotenv
+
+
+load_dotenv()
+
+
+# Get it from my.telegram.org
+API_ID = int(getenv("API_ID", ""))
+API_HASH = getenv("API_HASH")
+
+# Get it from @Botfather in Telegram.
+BOT_TOKEN = getenv("BOT_TOKEN")
